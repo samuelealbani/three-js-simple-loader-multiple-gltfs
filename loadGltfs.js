bc0f3ceeed0c5c54cf2dc1fe194b31a8ecf1d65b
rebecca-aston <rebecca.l.aston@gmail.com>
2024-01-25 11:03:31 +0000

    Update loadGltfs.js

--- loadGltfs.js
+++ loadGltfs.js
@@ -1,8 +1,9 @@
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 async function loadGltfs(_manager) {
-  const loader = new GLTFLoader(_manager);
+  const loader = new GLTFLoader(_manager);//pass in our loading manager
 
+  //add a variable name for each object in order of the files
   const [
     armChairGltf, carpetGltf, iridiscenceLampGltf,
      sheenchairGltf
